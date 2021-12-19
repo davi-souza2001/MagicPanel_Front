d999581f0c99112850285d3123f208cba837324d
davi-souza2001 <davisouza2001dv@gmail.com>
2021-12-19 20:50:24 -0300

    start function register

--- src/data/context/AuthContext.tsx
+++ src/data/context/AuthContext.tsx
@@ -1,17 +1,44 @@
+import client from '../../service/client'
+
 import { createContext, useState } from "react";
+import route from "next/router";
 
 interface AuthContextProps {
-    alo?: any;
+    register?: (user: UserRegister) => Promise<void> | undefined;
+}
+
+interface UserRegister {
+    name: String
+    email: String
+    password: String
+    confirmPassword: String
 }
 
 const AuthContext = createContext<AuthContextProps>({});
 
 export function AuthProvider(props: any){
+    const [authenticated, setAuthenticated] = useState(false)
 
-    const [alo, setAlo] = useState('PEGGGGGGGGGGGGGGGGGGOUY');
+    async function authUser(data: any) {
+        setAuthenticated(true)
+        localStorage.setItem('token', JSON.stringify(data.token))
+    
+        route.push('/')
+      }
+
+    async function register(user: UserRegister){
+        try {
+            const data = await client.post('/users/register', user).then((res) => {
+                return res.data
+            })
+            await authUser(data)
+        } catch (err){
+            console.log("Errou"+err)
+        }
+    }
 
     return(
-        <AuthContext.Provider value={{alo}}>
+        <AuthContext.Provider value={{register}}>
             {props.children}
         </AuthContext.Provider>
     )
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import useAuth from '../data/hook/useAuth'
 import styles from '../styles/Login.module.css'
 
@@ -5,22 +6,45 @@ interface Login {
     
 }
 
+interface User{
+    name: String
+    email: String
+    password: String
+    confirmPassword: String
+}
+
 
 export default function Login(props: Login){
-    const { alo } = useAuth()
+    const [registerLook, setRegisterLook] = useState(false)
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
 
-    console.log(alo)
+    const user: User = {name, email, password, confirmPassword}
+
+    const { register } = useAuth()
+
+    function handleSubmit(e: any){
+        e.preventDefault()
+        if(registerLook){
+            console.log(user)
+        }
+    }
+
 
     return (
         <div className={styles.contentGeral}>
             <div className={styles.contentBox}>
                 <h2>Login</h2>
-                <form action="">
-                    <input type="text" name="email" id="email" placeholder="Digite seu email" />
-                    <input type="text" name="password" id="password" placeholder="Digite sua senha" />
-                    <input type="submit" value="Logar"  />
+                <form onSubmit={handleSubmit}>
+                {registerLook && <input type="text" name="name" id="name" placeholder="Digite seu nome" onChange={(e) => setName(e.target.value)}/>}
+                    <input type="text" name="email" id="email" placeholder="Digite seu email" onChange={(e) => setEmail(e.target.value)}/>
+                    <input type="text" name="password" id="password" placeholder="Digite sua senha" onChange={(e) => setPassword(e.target.value)}/>
+                    {registerLook && <input type="text" name="confirmPassword" id="confirmPassword" placeholder="Digite sua confirmação de senha" onChange={(e) => setConfirmPassword(e.target.value)}/>}
+                    <input type="submit" value={registerLook ? 'Registrar' : 'Logar'}  />
                 </form>
-                <p>Não tem uma conta ? Clique aqui</p>
+                <p onClick={() => setRegisterLook(true)}>Não tem uma conta ? Clique aqui</p>
             </div>
         </div>
     )
